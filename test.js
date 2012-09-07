86dca3d7eaace9da1301a04ce28047fb1a4b97d1
Jason Zoo <zjcli@yahoo.cn>
2012-09-07 18:19:33 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,10 +1,1 @@
-var qilin = require("qilin"),
-	fs = require("fs");
-try {
-	var data = fs.readFileSync(__dirname + '/conf.json','utf-8');
-	var CONFIG = JSON.parse(data.toString());
-	qilin.start(CONFIG);
-}catch(e){
-	console.log("error" + e.message);
-}
 
